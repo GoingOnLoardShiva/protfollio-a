11631c2e06b14477b314f42a1bb14c4897eaf6d7
Your Name <your.email@example.com>
2025-12-30 20:18:59 +0530

    update

--- m-protfollio/src/app/layout.js
+++ m-protfollio/src/app/layout.js
@@ -5,7 +5,7 @@ import localFont from "next/font/local";
 // If you want to use Google fonts, add them via a <link> in metadata or use a supported font
 
 export const metadata = {
-  title: "Hiren Ray - Web App Developer",
+  title: "Hiren Ray | Full Stack Developer | MERN & Next.js",
   description: "Web App Developer",
 };
 const poppins = localFont({
@@ -29,9 +29,34 @@ const poppins = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased`}
-      >
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PZQQSTFF');
+            `,
+          }}
+        />
+      </head>
+      <body className={`${poppins.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PZQQSTFF"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
         {children}
       </body>
     </html>
